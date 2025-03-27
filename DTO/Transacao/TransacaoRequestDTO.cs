f89c804c3d74a5da9d0f1d695728242776b43cd5
Matheus Mendes <msmmendes211@gmail.com>
2025-03-27 14:42:39 -0300

    feat: adiciona DTOs para transação

---
+++ DTO/Transacao/TransacaoRequestDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using ControleDeGastos.Model;
+
+namespace ControleDeGastos.DTO.Transacao
+{
+    public record TransacaoRequestDTO(
+        [Required(ErrorMessage = "Informe a descrição")]
+        string descricao,
+
+        [Required(ErrorMessage = "Informe o valor")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "O valor deve ser maior do que 0")]
+        double valor,
+
+        [Required(ErrorMessage = "Informe o tipo")]
+        TransacaoTipo tipo
+    );
+}
---
+++ DTO/Transacao/TransacaoResponseDTO.cs
@@ -0,0 +1,10 @@
+﻿using ControleDeGastos.Model;
+
+namespace ControleDeGastos.DTO.Transacao
+{
+    public record TransacaoResponseDTO(
+        string descricao,
+        double valor,
+        TransacaoTipo tipo
+    );
+}
