05e58dcf6e2f288376bb39caf7ab1c4a9b87c402
Matheus Mendes <msmmendes211@gmail.com>
2025-03-27 14:13:04 -0300

    feat: adiciona DTO PessoaMenorComTotais

---
+++ DTO/Pessoa/PessoaMenorComTotaisDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace ControleDeGastos.DTO.Pessoa
+{
+    public record PessoaMenorComTotaisDTO(
+        long Id,
+        string Nome,
+        int Idade,
+        double Mesada,
+        double Despesas,
+        double Saldo
+    );
+}
