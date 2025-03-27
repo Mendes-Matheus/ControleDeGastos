ebb44308d82c29c54ffd9246755cfd7b2af433b8
Matheus Mendes <msmmendes211@gmail.com>
2025-03-27 01:26:27 -0300

    feat: adiciona PessoaController

---
+++ Controllers/PessoaController.cs
@@ -0,0 +1,29 @@
+﻿using ControleDeGastos.DTO.Pessoa;
+using ControleDeGastos.Model;
+using ControleDeGastos.Service.Pessoa;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControleDeGastos.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PessoaController : Controller
+    {
+
+
+        private readonly IPessoaServiceInterface _pessoaServiceInterface;
+
+        public PessoaController(IPessoaServiceInterface pessoaServiceInterface)
+        {
+            _pessoaServiceInterface = pessoaServiceInterface;
+        }
+
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
