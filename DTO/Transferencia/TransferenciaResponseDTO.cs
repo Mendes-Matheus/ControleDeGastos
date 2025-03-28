b03aecd0ca1c69e9512a05e5f443d33866aad026
Matheus Mendes <msmmendes211@gmail.com>
2025-03-28 14:28:12 -0300

    feat: adiciona TransferenciaResponseDTO

---
+++ DTO/Transferencia/TransferenciaResponseDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace ControleDeGastos.DTO.Transferencia
+{
+    public record TransferenciaResponseDTO(
+        string descricao,
+        string remetente,
+        string destinatario,
+        string dataHora,
+        double valor
+    );
+    
+}
